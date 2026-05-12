ec14fcbcc101c25a8933ce83321696f06007f237
John Ray <johnraycaete@gmail.com>
2026-05-12 15:07:10 +0800

    feat: add DI for workspace

--- src/container.ts
+++ src/container.ts
@@ -1,13 +1,20 @@
 import { UserRepository } from "./modules/user/user.repository";
 import { UserService } from "./modules/user/user.service";
 import { UserController } from "./modules/user/user.controller";
+import { WorkspaceRepository } from "./modules/workspace/workspace.repository";
+import { WorkspaceController } from "./modules/workspace/workspace.controller";
+import { WorkspaceService } from "./modules/workspace/workspace.service";
+
 import { prisma } from "./lib/prisma";
 
 // repository
 const userRepo = new UserRepository(prisma)
+const workspaceRepo = new WorkspaceRepository(prisma)
 
 // services
 const userService = new UserService(userRepo)
+const workspaceService = new WorkspaceService(workspaceRepo, prisma)
 
 // controller
 export const userController = new UserController(userService)
+export const workspaceController = new WorkspaceController(workspaceService)
