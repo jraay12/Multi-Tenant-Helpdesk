9164e2c4ead2510d0a18b109f5c0391fc77ac226
John Ray <johnraycaete@gmail.com>
2026-05-12 15:07:18 +0800

    feat: register workspace routes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import { errorHandler } from "./shared/middleware/errorHandler";
 import userRoutes from "./modules/user/user.routes";
-import { userController } from "./container";
+import { userController, workspaceController } from "./container";
+import workspaceRoutes from "./modules/workspace/workspace.routes";
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/users", userRoutes(userController))
+app.use("/api/v1/users", userRoutes(userController));
+app.use("/api/v1/workspace", workspaceRoutes(workspaceController));
 
-app.use(errorHandler)
+
+app.use(errorHandler);
 
 app.get("/health", (req, res) => {
   res.send("Express + TypeScript is running 🚀");
 });
 
-export default app
+export default app;
