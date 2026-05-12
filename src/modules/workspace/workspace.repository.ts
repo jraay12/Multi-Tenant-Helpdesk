4bdaed0c02f241e1381d568539b5ad8eae813619
John Ray <johnraycaete@gmail.com>
2026-05-12 15:19:19 +0800

    feat: add method findBySlug

--- src/modules/workspace/workspace.repository.ts
+++ src/modules/workspace/workspace.repository.ts
@@ -6,14 +6,16 @@ import {
   PrismaClient as PrismaClientType,
 } from "@prisma/client";
 
-
-type PrismaTx = Prisma.TransactionClient
+type PrismaTx = Prisma.TransactionClient;
 
 export class WorkspaceRepository {
   constructor(private prisma: PrismaClientType) {}
 
-  async save(data: Prisma.WorkspaceCreateInput, tx?: PrismaTx): Promise<Workspace> {
-    const client = tx ?? this.prisma
+  async save(
+    data: Prisma.WorkspaceCreateInput,
+    tx?: PrismaTx,
+  ): Promise<Workspace> {
+    const client = tx ?? this.prisma;
     return await client.workspace.create({
       data,
     });
@@ -27,12 +29,24 @@ export class WorkspaceRepository {
     });
   }
 
-  async addMember(data: {
-    userId: string;
-    workspaceId: string;
-    role: "OWNER" | "ADMIN" | "AGENT" | "VIEWER";
-  }, tx?: PrismaTx): Promise<WorkspaceMember> {
-    const client = tx ?? this.prisma
+  async findBySlug(slug: string, tx?: PrismaTx,): Promise<Workspace | null> {
+     const client = tx ?? this.prisma;
+    return await client.workspace.findUnique({
+      where: {
+        slug,
+      },
+    });
+  }
+
+  async addMember(
+    data: {
+      userId: string;
+      workspaceId: string;
+      role: "OWNER" | "ADMIN" | "AGENT" | "VIEWER";
+    },
+    tx?: PrismaTx,
+  ): Promise<WorkspaceMember> {
+    const client = tx ?? this.prisma;
     return client.workspaceMember.create({
       data,
     });
