c95f501ca0e20d7db30d841cb2ee334d43800fe8
John Ray <johnraycaete@gmail.com>
2026-05-12 17:26:08 +0800

    DI the ticket modules

--- src/container.ts
+++ src/container.ts
@@ -4,17 +4,21 @@ import { UserController } from "./modules/user/user.controller";
 import { WorkspaceRepository } from "./modules/workspace/workspace.repository";
 import { WorkspaceController } from "./modules/workspace/workspace.controller";
 import { WorkspaceService } from "./modules/workspace/workspace.service";
-
+import { TicketController } from "./modules/ticket/ticket.controller";
+import { TicketRepository } from "./modules/ticket/ticket.repository";
+import { TicketService } from "./modules/ticket/ticket.service";
 import { prisma } from "./lib/prisma";
 
 // repository
 export const userRepo = new UserRepository(prisma)
 const workspaceRepo = new WorkspaceRepository(prisma)
-
+const ticketRepo = new TicketRepository(prisma)
 // services
 const userService = new UserService(userRepo)
 const workspaceService = new WorkspaceService(workspaceRepo, prisma)
+const ticketService = new TicketService(ticketRepo, workspaceRepo, prisma)
 
 // controller
 export const userController = new UserController(userService)
 export const workspaceController = new WorkspaceController(workspaceService)
+export const ticketController = new TicketController(ticketService)
