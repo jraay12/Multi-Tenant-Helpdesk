d89655ba092eba2ac2983e42847ff8c563bbb245
John Ray <johnraycaete@gmail.com>
2026-05-12 17:25:07 +0800

    add ticket validations

---
+++ src/modules/ticket/ticket.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const CreateTicketSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters")
+    .max(100, "Title must be at most 100 characters"),
+
+  description: z
+    .string()
+    .max(1000, "Description too long")
+    .optional(),
+
+  priority: z.enum(["LOW", "MEDIUM", "HIGH", "URGENT"]).optional(),
+  category: z
+    .string()
+    .max(1000, "Description too long")
+    .optional(),
+});
