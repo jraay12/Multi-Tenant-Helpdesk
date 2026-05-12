53baa0a6905b65317b3fd8fda1f1c82971f16e27
John Ray <johnraycaete@gmail.com>
2026-05-12 14:26:57 +0800

    feat: register user routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { errorHandler } from "./shared/middleware/errorHandler";
+import userRoutes from "./modules/user/user.routes";
+import { userController } from "./container";
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/users", userRoutes(userController))
 
 app.use(errorHandler)
 
