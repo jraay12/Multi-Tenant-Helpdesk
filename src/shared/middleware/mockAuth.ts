e17571f0cff13f5e00a611211d183ba9aeee556a
John Ray <johnraycaete@gmail.com>
2026-05-12 15:13:05 +0800

    feat: add checker for user id

--- src/shared/middleware/mockAuth.ts
+++ src/shared/middleware/mockAuth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-
+import { userRepo } from "../../container";
+import { UnAuthorized } from "../errors/UnAuthorized";
 declare global {
   namespace Express {
     interface Request {
@@ -9,13 +10,18 @@ declare global {
     }
   }
 }
-export function mockAuth(req: Request, res: Response, next: NextFunction) {
+export async function mockAuth(req: Request, res: Response, next: NextFunction) {
   const userId = req.headers["x-user-id"];
 
   if (!userId || typeof userId !== "string") {
     return res.status(401).json({
       message: "Unauthorized: missing x-user-id header",
     });
+  }
+
+  const user = await userRepo.findById(userId);
+  if (!user) {
+    throw new UnAuthorized("Unauthorized: user not found")
   }
 
   (req as any).user = {
