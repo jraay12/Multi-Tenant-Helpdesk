112d6bf3b8a549e4d6b30984578a0896c9ef252f
John Ray <johnraycaete@gmail.com>
2026-05-12 17:25:32 +0800

    add ticket controller

---
+++ src/modules/ticket/ticket.controller.ts
@@ -0,0 +1,26 @@
+import { TicketService } from "./ticket.service";
+import { CreateTicketSchema } from "./ticket.validation";
+import { Request, Response, NextFunction } from "express";
+
+export class TicketController {
+  constructor(private ticketService: TicketService) {}
+
+  createTicket = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const parsed = CreateTicketSchema.parse(req.body);
+
+      const ticket = await this.ticketService.createTicket(
+        req.user!.id,
+        req.workspaceId!,
+        parsed,
+      );
+
+      return res.status(201).json({
+        message: "Ticket created successfully",
+        data: ticket,
+      });
+    } catch (err: any) {
+      next(err)
+    }
+  };
+}
