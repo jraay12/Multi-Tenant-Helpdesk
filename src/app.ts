b6e48fb5f277e1cf7545d3ba7cd0d304002a81b3
John Ray <johnraycaete@gmail.com>
2026-05-12 13:43:49 +0800

    feat: register the error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
-
+import { errorHandler } from "./shared/middleware/errorHandler";
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandler)
 
 app.get("/health", (req, res) => {
   res.send("Express + TypeScript is running 🚀");
