8fe4ee6e7dcbcf001682adea500273f28da86981
John Ray <johnraycaete@gmail.com>
2026-05-12 17:25:40 +0800

    add ticket routes

---
+++ src/modules/ticket/ticket.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express"
+import { TicketController } from "./ticket.controller"
+import { mockAuth } from "../../shared/middleware/mockAuth"
+import { workspaceContext } from "../../shared/middleware/workspaceContext"
+
+const ticketRoute = (ticketController: TicketController): Router => {
+  const routes = Router()
+
+  routes.post("/", mockAuth, workspaceContext, ticketController.createTicket)
+  
+  return routes
+
+}
+
+export default ticketRoute
