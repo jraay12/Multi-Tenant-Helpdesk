9eb31570151bb717229a633a8f4b3d2ea68bacba
John Ray <johnraycaete@gmail.com>
2026-05-12 16:30:22 +0800

    feat: add switch work space in controller

--- src/modules/workspace/workspace.controller.ts
+++ src/modules/workspace/workspace.controller.ts
@@ -7,9 +7,11 @@ export class WorkspaceController {
 
   createWorkspace = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const {name} = CreateWorkspaceSchema.parse(req.body)
+      const { name } = CreateWorkspaceSchema.parse(req.body);
       const userId = req.user!.id;
-      const result = await this.workspaceService.createWorkspace(userId, {name});
+      const result = await this.workspaceService.createWorkspace(userId, {
+        name,
+      });
       res.status(201).json(result);
     } catch (error) {
       next(error);
@@ -26,12 +28,36 @@ export class WorkspaceController {
     }
   };
 
-  getWorkspaceById = async (req: Request, res: Response, next: NextFunction) => {
+  getWorkspaceById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const userId = req.user!.id;
-      const {workspace_id} = req.params as {workspace_id: string}
-      const result = await this.workspaceService.getWorkspaceById(userId, workspace_id);
+      const { workspace_id } = req.params as { workspace_id: string };
+      const result = await this.workspaceService.getWorkspaceById(
+        userId,
+        workspace_id,
+      );
       res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  switchWorkspace = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user!.id;
+      const { workspace_id } = req.params as { workspace_id: string };
+      const result = await this.workspaceService.switchWorkspace(
+        userId,
+        workspace_id,
+      );
+      return res.json({
+        message: "Workspace switched successfully",
+        data: result,
+      });
     } catch (error) {
       next(error);
     }
