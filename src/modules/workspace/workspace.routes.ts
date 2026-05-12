8eb7f8484f5333bbd3e7e443f3123986b94d68e3
John Ray <johnraycaete@gmail.com>
2026-05-12 16:30:52 +0800

    feat: add switch workspace in routes

--- src/modules/workspace/workspace.routes.ts
+++ src/modules/workspace/workspace.routes.ts
@@ -1,17 +1,15 @@
-import { Router } from "express"
-import { WorkspaceController } from "./workspace.controller"
-import { mockAuth } from "../../shared/middleware/mockAuth"
+import { Router } from "express";
+import { WorkspaceController } from "./workspace.controller";
+import { mockAuth } from "../../shared/middleware/mockAuth";
 const workspaceRoutes = (workspaceController: WorkspaceController): Router => {
-  const routes = Router()
+  const routes = Router();
 
-  routes.post("/", mockAuth, workspaceController.createWorkspace)
-  routes.get("/", mockAuth, workspaceController.getMyWorkspace)
-  routes.get("/:workspace_id", mockAuth, workspaceController.getWorkspaceById)
+  routes.post("/", mockAuth, workspaceController.createWorkspace);
+  routes.get("/", mockAuth, workspaceController.getMyWorkspace);
+  routes.get("/:workspace_id", mockAuth, workspaceController.getWorkspaceById);
+  routes.post("/:workspace_id/switch", mockAuth, workspaceController.switchWorkspace);
 
+  return routes;
+};
 
-  
-  return routes
-
-}
-
-export default workspaceRoutes
+export default workspaceRoutes;
