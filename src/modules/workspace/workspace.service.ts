01bef05734236106464d64e2bd5cfddae96d0ed8
John Ray <johnraycaete@gmail.com>
2026-05-12 15:19:36 +0800

    feat: handle usecase where slug is the same

--- src/modules/workspace/workspace.service.ts
+++ src/modules/workspace/workspace.service.ts
@@ -4,25 +4,41 @@ import { generateSlug } from "../../shared/utils/generateSlug";
 import { CreateWorkspaceDTO } from "./workspace.types";
 
 export class WorkspaceService {
-  constructor(private workspaceRepo: WorkspaceRepository, private prisma: PrismaClient){}
-
+  constructor(
+    private workspaceRepo: WorkspaceRepository,
+    private prisma: PrismaClient,
+  ) {}
 
   async createWorkspace(userId: string, data: CreateWorkspaceDTO) {
     return this.prisma.$transaction(async (tx) => {
-      const slug = generateSlug(data.name)
+      const baseSlug = generateSlug(data.name);
 
-      const workspace = await this.workspaceRepo.save({
-        name: data.name,
-        slug
-      }, tx)
+      let slug = baseSlug;
+      let counter = 1;
 
-      await this.workspaceRepo.addMember({
-        userId,
-        role: "OWNER",
-        workspaceId: workspace.id
-      }, tx)
+      while (await this.workspaceRepo.findBySlug(slug)) {
+        slug = `${baseSlug}-${counter}`;
+        counter++;
+      }
 
-      return workspace
-    })
+      const workspace = await this.workspaceRepo.save(
+        {
+          name: data.name,
+          slug,
+        },
+        tx,
+      );
+
+      await this.workspaceRepo.addMember(
+        {
+          userId,
+          role: "OWNER",
+          workspaceId: workspace.id,
+        },
+        tx,
+      );
+
+      return workspace;
+    });
   }
 }
