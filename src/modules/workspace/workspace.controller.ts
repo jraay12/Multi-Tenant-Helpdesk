88f8d4b94e208b9daecced0641e429b361983b0e
John Ray <johnraycaete@gmail.com>
2026-05-12 15:06:28 +0800

    feat: add method for create workspace

---
+++ src/modules/workspace/workspace.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { CreateWorkspaceSchema } from "./workspace.validation";
+import { WorkspaceService } from "./workspace.service";
+
+export class WorkspaceController {
+  constructor(private workspaceService: WorkspaceService) {}
+
+  createWorkspace = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const {name} = CreateWorkspaceSchema.parse(req.body)
+      const userId = req.user!.id;
+      const result = await this.workspaceService.createWorkspace(userId, {name});
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
