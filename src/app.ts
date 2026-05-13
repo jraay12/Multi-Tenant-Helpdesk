ede748b582a9fc20aa8f6c0bd4abe3f76ac21566
John Ray <johnraycaete@gmail.com>
2026-05-13 15:42:05 +0800

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors, { CorsOptions } from 'cors';
 import { errorHandler } from "./shared/middleware/errorHandler";
 import userRoutes from "./modules/user/user.routes";
 import {
@@ -12,6 +13,12 @@ import ticketRoute from "./modules/ticket/ticket.routes";
 import authRoutes from "./modules/auth/auth.routes";
 const app = express();
 
+const corsOptions: CorsOptions = {
+ origin: ['http://localhost:5173'],
+ methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"],
+ credentials: true,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes(userController));
